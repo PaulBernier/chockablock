9a70cafcd549d2a45a1fe3c00082ed11c0bfc8b0
Paul Bernier <bernier.pja@gmail.com>
2020-04-09 14:02:24 -0700

    refactor: lintGQL set to true

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 
 module.exports = {
+  pluginOptions: { apollo: { lintGQL: true } },
   configureWebpack: () => {
     const API_BASE_URL =
       process.env.NODE_ENV === "production" ? "" : "http://localhost:4000";
@@ -8,9 +9,9 @@ module.exports = {
     return {
       plugins: [
         new webpack.DefinePlugin({
-          "process.env.API_BASE_URL": JSON.stringify(API_BASE_URL)
-        })
-      ]
+          "process.env.API_BASE_URL": JSON.stringify(API_BASE_URL),
+        }),
+      ],
     };
-  }
+  },
 };
